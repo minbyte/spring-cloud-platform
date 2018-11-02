64862b85f4dae6fa8e43fdc18e41c51acbbd1988
huangmin <huangmin@mgtv.com>
2018-11-02 17:34:41 +0800

    consul-client
    Change-Id: Ic0946a061dfd03bc15d6599d43cc5c48e0de6b21

---
+++ registry-center/consul-client/src/main/java/com/mindasoft/eureka/client/ConsulClientApplication.java
@@ -0,0 +1,16 @@
+package com.mindasoft.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsulClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulClientApplication.class, args);
+    }
+
+}
