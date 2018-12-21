6df50970a07704c487aeeca0fccb55b1c7eda9f3
huangmin <huangmin@mgtv.com>
2018-12-21 16:32:16 +0800

    scp_security
    Change-Id: I6a03cd21b7242841e2d583441ecb5b9dd77e0174

---
+++ security-center/doc/scp_security.sql
@@ -0,0 +1,76 @@
+/*
+SQLyog  v12.2.6 (64 bit)
+MySQL - 5.6.39 : Database - scp_security
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE `scp_security`;
+
+/*Table structure for table `oauth_access_token` */
+
+DROP TABLE IF EXISTS `oauth_access_token`;
+
+CREATE TABLE `oauth_access_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(100) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  `authentication` blob,
+  `refresh_token` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `oauth_access_token` */
+
+/*Table structure for table `oauth_client_details` */
+
+DROP TABLE IF EXISTS `oauth_client_details`;
+
+CREATE TABLE `oauth_client_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `client_id` varchar(48) NOT NULL COMMENT '应用标识',
+  `resource_ids` varchar(256) DEFAULT NULL COMMENT '资源限定串(逗号分割)',
+  `client_secret` varchar(256) DEFAULT NULL COMMENT '应用密钥(bcyt) 加密',
+  `client_secret_str` varchar(256) DEFAULT NULL COMMENT '应用密钥(明文)',
+  `scope` varchar(256) DEFAULT NULL COMMENT '范围',
+  `authorized_grant_types` varchar(256) DEFAULT NULL COMMENT '5种oauth授权方式(authorization_code,password,refresh_token,client_credentials)',
+  `web_server_redirect_uri` varchar(256) DEFAULT NULL COMMENT '回调地址 ',
+  `authorities` varchar(256) DEFAULT NULL COMMENT '权限',
+  `access_token_validity` int(11) DEFAULT NULL COMMENT 'access_token有效期',
+  `refresh_token_validity` int(11) DEFAULT NULL COMMENT 'refresh_token有效期',
+  `additional_information` varchar(4096) DEFAULT '{}' COMMENT '{}',
+  `autoapprove` varchar(256) DEFAULT NULL COMMENT '是否自动授权 是-true',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `oauth_client_details` */
+
+insert  into `oauth_client_details`(`id`,`client_id`,`resource_ids`,`client_secret`,`client_secret_str`,`scope`,`authorized_grant_types`,`web_server_redirect_uri`,`authorities`,`access_token_validity`,`refresh_token_validity`,`additional_information`,`autoapprove`) values
+  (1,'app',NULL,'$2a$10$i3F515wEDiB4Gvj9ym9Prui0dasRttEUQ9ink4Wpgb4zEDCAlV8zO','app','app','password,refresh_token',NULL,NULL,180000,NULL,'{}','true'),
+  (2,'webApp',NULL,'$2a$10$06msMGYRH8nrm4iVnKFNKOoddB8wOwymVhbUzw/d3ZixD7Nq8ot72','webApp','webApp','authorization_code,password,refresh_token,client_credentials',NULL,NULL,180000,NULL,'{}','true');
+
+/*Table structure for table `oauth_refresh_token` */
+
+DROP TABLE IF EXISTS `oauth_refresh_token`;
+
+CREATE TABLE `oauth_refresh_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `oauth_refresh_token` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
