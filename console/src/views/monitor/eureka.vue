5e07df87083d592ab9e3074103d635b5514f66a4
huangmin <huangmin@mgtv.com>
2019-01-04 16:07:39 +0800

    添加xxl-job
    Change-Id: Ic8c6d9d7bf9d7193842a5b05c49038263a968947

---
+++ console/src/views/monitor/eureka.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="app-container">
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dataForm: {
+        key: ''
+      },
+      dataList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0,
+      dataListLoading: false,
+      dataListSelections: [],
+      addOrUpdateVisible: false
+    }
+  },
+  created (){
+    this.getDataList()
+  },
+  methods: {
+    // 获取数据列表
+    getDataList () {
+      this.dataListLoading = true
+      this.$http({
+        url: '/eureka/eureka/apps',
+        method: 'get',
+        params: {
+          'page': this.pageIndex,
+          'limit': this.pageSize,
+          'key': this.dataForm.key
+        }
+      }).then(response => {
+        if (response && response.code === 0) {
+          this.dataList = response.data.list
+          this.totalPage = response.data.totalCount
+        } else {
+          this.dataList = []
+          this.totalPage = 0
+        }
+        this.dataListLoading = false
+      })
+    },
+    // 每页数
+    sizeChangeHandle (val) {
+      this.pageSize = val
+      this.pageIndex = 1
+      this.getDataList()
+    },
+    // 当前页
+    currentChangeHandle (val) {
+      this.pageIndex = val
+      this.getDataList()
+    },
+    // 多选
+    selectionChangeHandle (val) {
+      this.dataListSelections = val
+    },
+    // 删除
+    deleteHandle (id) {
+      var ids = id ? [id] : this.dataListSelections.map(item => {
+        return item.userId
+      })
+      this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: '/users/user/delete',
+          method: 'delete',
+          data: ids
+        }).then(response => {
+          if (response && response.code === 0) {
+            this.$message({
+              message: '操作成功',
+              type: 'success',
+              duration: 1500,
+              onClose: () => {
+                this.getDataList()
+              }
+            })
+          } else {
+            this.$message.error(response.msg)
+          }
+        })
+      })
+    }
+  }
+}
+</script>
