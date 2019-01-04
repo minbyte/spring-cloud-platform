668f1100423d3d16154fbab33e5b41740cd98404
huangmin <huangmin@mgtv.com>
2019-01-04 17:28:19 +0800

    去掉验证登录
    Change-Id: I3ef67689d0548a0aa8672c7b2dc6f50b7bc07ff5

--- job-center/job-admin/src/main/java/com/xxl/job/admin/controller/interceptor/WebMvcConfig.java
+++ job-center/job-admin/src/main/java/com/xxl/job/admin/controller/interceptor/WebMvcConfig.java
@@ -14,14 +14,14 @@ import javax.annotation.Resource;
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    @Resource
-    private PermissionInterceptor permissionInterceptor;
+//    @Resource
+//    private PermissionInterceptor permissionInterceptor;
     @Resource
     private CookieInterceptor cookieInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(permissionInterceptor).addPathPatterns("/**");
+//        registry.addInterceptor(permissionInterceptor).addPathPatterns("/**");
         registry.addInterceptor(cookieInterceptor).addPathPatterns("/**");
         super.addInterceptors(registry);
     }
