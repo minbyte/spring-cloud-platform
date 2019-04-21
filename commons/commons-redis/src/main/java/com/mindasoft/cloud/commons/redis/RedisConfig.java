32dde919fc10fdc4cc2fa43389b619a84ce14c79
min <hmiter@sina.com>
2019-04-21 16:47:08 +0800

    RedisObjectSerializer.java

--- commons/commons-redis/src/main/java/com/mindasoft/cloud/commons/redis/RedisConfig.java
+++ commons/commons-redis/src/main/java/com/mindasoft/cloud/commons/redis/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
@@ -22,11 +23,15 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(factory);
+
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        RedisSerializer redisObjectSerializer = new RedisObjectSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        redisTemplate.setHashValueSerializer(stringSerializer);
+        redisTemplate.setValueSerializer(redisObjectSerializer);
+        redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
 
